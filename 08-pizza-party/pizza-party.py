2ebb646f579ac7e27d4584d10eb9b608b8c06821
Ville Viklund <ville.viklund@morgan.fi>
2018-04-08 15:18:22 +0300

    Add exercise 08

---
+++ 08-pizza-party/pizza-party.py
@@ -0,0 +1,10 @@
+people = input('How many people? ')
+pizza_count = input('How many pizzas do you have? ')
+slices_per_pizza = input('How many slices per pizza? ')
+slice_count = int(pizza_count) * int(slices_per_pizza)
+slices_per_person = slice_count // int(people)
+leftover_slices = slice_count - slices_per_person * int(people) 
+
+print(str(people), 'people with', str(pizza_count), 'pizzas')
+print(f"Each person gets {slices_per_person} pieces of pizza.")
+print(f"There are {leftover_slices} leftover pieces.")
