6257bf56842803ae757e1ac16c71fae229eb4c90
Ville Viklund <ville.viklund@morgan.fi>
2018-04-08 16:03:04 +0300

    Allow only numerical values

--- 08-pizza-party/pizza-party.py
+++ 08-pizza-party/pizza-party.py
@@ -1,6 +1,15 @@
-people = input('How many people? ')
-pizza_count = input('How many pizzas do you have? ')
-slices_per_pizza = input('How many slices per pizza? ')
+def ask_int(question):
+    answer = ''
+
+    while answer.isdigit() == False:
+        answer = input(question)
+
+    return answer
+
+people = ask_int('How many people? ')
+pizza_count = ask_int('How many pizzas do you have? ')
+slices_per_pizza = ask_int('How many slices per pizza? ')
+
 slice_count = int(pizza_count) * int(slices_per_pizza)
 slices_per_person = slice_count // int(people)
 leftover_slices = slice_count - slices_per_person * int(people) 
