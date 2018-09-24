0076222203f4a09afe9f7954905b36355171dccb
Ville Viklund <ville.viklund@morgan.fi>
2018-09-24 10:07:26 +0300

    Refactor functionality to a function

--- 12-computing-simple-interest/computing-simple-interest.py
+++ 12-computing-simple-interest/computing-simple-interest.py
@@ -1,10 +1,12 @@
 import locale
 locale.setlocale( locale.LC_ALL, 'en_CA.UTF-8' )
 
+def calculate_simple_interest(principal, rate_of_interest, years):
+    return int(principal) * (1 + (float(rate_of_interest) / 100) * int(years))
+
 principal = input('Enter the principal: ')
 rate_of_interest = input('Enter the rate of interest: ')
 years = input('Enter the number of years: ')
-
-worth = int(principal) * (1 + (float(rate_of_interest) / 100) * int(years))
+worth = calculate_simple_interest(principal, rate_of_interest, years)
 
 print(locale.currency(worth))
